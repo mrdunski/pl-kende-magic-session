d4542b7460517fc2b7c3867e8b749df741d4a55f
Patryk Dunski <patryk.dunski@kende.pl>
2013-12-02 20:36:49 +0100

    3. Session handling code

--- grails-app/controllers/pl/kende/magic/session/MagicSessionController.groovy
+++ grails-app/controllers/pl/kende/magic/session/MagicSessionController.groovy
@@ -1,6 +1,22 @@
 package pl.kende.magic.session
 
+import javax.servlet.http.HttpServletResponse;
+
 class MagicSessionController {
 
-    def index() { }
+	def sessionManagerService
+
+	def index() {
+		render view: 'site'
+	}
+
+	def session() {
+		def oldSessionId = request.getHeader('If-None-Match')
+		def newSessionId = sessionManagerService.registerOldSession(oldSessionId)
+		if (oldSessionId == newSessionId) {
+			render status: HttpServletResponse.SC_NOT_MODIFIED
+		} else {
+			render text: "\$.ajaxSetup({headers:{\"Kende-Session-Id\": \"${newSessionId}\"}})", contentType: "application/javascript"
+		}
+	}
 }
